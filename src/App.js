1e281e702c40693a738fb75b443d31ce8884d83a
Vladyslav Prozapas <vproz@softserveinc.com>
2018-06-27 12:57:36 +0300

    Moved router to separate container, created checkout route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import combinedStore from './reducers/';
 import rootSaga from './sagas';
 
 import Translations from './containers/Translations';
+import Router from './containers/Router';
 
 import './styles/index.css';
-
-import Home from './screens/Home';
-import Products from './screens/Products';
-import Product from './screens/Product';
-import Cart from './screens/Cart';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(combinedStore, applyMiddleware(sagaMiddleware));
@@ -26,14 +21,7 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <Translations>
-                    <BrowserRouter>
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/products" component={Products} />
-                            <Route path="/products/:id" component={Product} />
-                            <Route path="/cart" component={Cart} />
-                        </Switch>
-                    </BrowserRouter>
+                    <Router />
                 </Translations>
             </Provider>
         );
---
+++ src/containers/Router.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Home from '../screens/Home';
+import Products from '../screens/Products';
+import Product from '../screens/Product';
+import Cart from '../screens/Cart';
+
+import { getCartState } from '../selectors';
+
+class Router extends React.PureComponent {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/products" component={Products} />
+                    <Route path="/products/:id" component={Product} />
+                    <Route exact path="/cart" component={Cart} />
+                    <Route path ="/cart/checkout" render={() => (
+                        this.props.cartProducts.length ? (
+                            <Cart />
+                        ) : (
+                            <Redirect to="/products" />
+                        )
+                    )} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+Router.proptypes = {
+    cartProducts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => {
+    return {
+        cartProducts: getCartState(state),
+    }
+};
+
+export default connect(mapStateToProps, null)(Router);
